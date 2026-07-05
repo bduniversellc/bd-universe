55b136507f4adc596b15f22a56667f8db6bc09a9
bduniversellc <help@bduniverse.com>
2026-07-04 21:36:55 -0500

    Add files via upload

---
+++ apps/bdui-lab/assets/js/lab.js
@@ -0,0 +1,16 @@
+/*
+====================================================
+BD Universe
+
+BDUI Lab
+
+Version: 1.0.0
+
+====================================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("BDUI Lab Loaded");
+
+});
