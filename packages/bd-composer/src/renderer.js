ccc4d9f3e0860513fa6faea87562c07225119d61
bduniversellc <help@bduniverse.com>
2026-07-05 18:36:21 -0500

    Add files via upload

---
+++ packages/bd-composer/src/renderer.js
@@ -0,0 +1,31 @@
+/*
+====================================================
+
+BD Universe
+
+Package : BD Composer
+File    : renderer.js
+Version : 1.1.0
+
+====================================================
+*/
+
+export class Renderer {
+
+    static initialize(){
+
+        console.log("Renderer Ready");
+
+    }
+
+    static render(component, target){
+
+        console.log("Rendering");
+
+        console.log(component);
+
+        console.log(target);
+
+    }
+
+}
