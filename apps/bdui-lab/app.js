1697613daff157ee51a2aa78eb8efd7cfc9f7b28
bduniversellc <help@bduniverse.com>
2026-07-07 07:06:15 -0500

    Add files via upload

---
+++ apps/bdui-lab/app.js
@@ -0,0 +1,25 @@
+/*
+====================================================
+
+BD Studio
+
+Application Bootstrap
+
+====================================================
+*/
+
+console.clear();
+
+console.log("======================================");
+
+console.log("BD Studio Alpha");
+
+console.log("Version 0.1.0");
+
+console.log("======================================");
+
+const status = document.querySelector(".statusbar");
+
+status.children[1].textContent = "Kernel: Ready";
+
+console.log("Application Loaded");
