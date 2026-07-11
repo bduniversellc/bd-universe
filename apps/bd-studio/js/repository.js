d7a65063459919a552da9482b54705a6a7da4e34
borislovehelp <borislovehelp@gmail.com>
2026-07-11 10:04:51 -0500

    Update repository.js

--- apps/bd-studio/js/repository.js
+++ apps/bd-studio/js/repository.js
@@ -1,5 +1,62 @@
+/*
+======================================
+
+BD Studio
+Repository Module
+
+Version 0.8.3
+
+======================================
+*/
+
 export const repository = [
 
-    ...
+    {
+        name: "apps",
+        children: [
+            "bd-studio",
+            "bdui-lab"
+        ]
+    },
+
+    {
+        name: "core",
+        children: [
+            "bd-kernel",
+            "services"
+        ]
+    },
+
+    {
+        name: "packages",
+        children: [
+            "bdui"
+        ]
+    },
+
+    {
+        name: "blueprints",
+        children: []
+    },
+
+    {
+        name: "docs",
+        children: []
+    },
+
+    {
+        name: "knowledge",
+        children: []
+    },
+
+    {
+        name: "tests",
+        children: []
+    },
+
+    {
+        name: "tools",
+        children: []
+    }
 
 ];
