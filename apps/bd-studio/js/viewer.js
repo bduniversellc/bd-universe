3934d4e0fa1edae6962339d5a9e6bd717699d59f
borislovehelp <borislovehelp@gmail.com>
2026-07-12 00:21:37 -0500

    Update viewer.js

--- apps/bd-studio/js/viewer.js
+++ apps/bd-studio/js/viewer.js
@@ -4,7 +4,7 @@
 BD Studio
 Viewer Module
 
-Version 1.1.0
+Version 1.2.4
 
 ======================================
 */
@@ -28,5 +28,41 @@ ${content}
         </div>
 
     `;
+
+}
+
+export function showWelcome() {
+
+    return renderViewer(
+
+        "Welcome",
+
+`Welcome to BD Studio
+
+The development workspace for BD Universe.
+
+Select a file from the Explorer to view its contents.
+
+Version: 1.2.4`
+
+    );
+
+}
+
+export function showFile(file) {
+
+    return renderViewer(
+
+        file.name,
+
+`File: ${file.name}
+
+Path: ${file.path}
+
+Type: ${file.type}
+
+Preview support coming soon...`
+
+    );
 
 }
